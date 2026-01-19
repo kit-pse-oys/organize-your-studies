7cf83049f7f63baca29bfebc66357c210721f9da
Dav <davadamdebler123@gmail.com>
2026-01-19 17:18:16 +0100

    Initialize Java server project with Gradle setup and basic application structure

---
+++ java-server/src/test/java/de/pse/oys/javaserver/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pse.oys.javaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
