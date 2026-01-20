043f678bf516cde8fd761ac935c2d052e235a030
Dav <davadamdebler123@gmail.com>
2026-01-20 10:03:52 +0100

    Initialer SmokeTest hinzugefügt, beabsichtigt auf fail() zum Test der CI Pipeline

---
+++ java-server/src/test/java/de/pse/oys/SmokeTest.java
@@ -0,0 +1,19 @@
+package de.pse.oys;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * SmokeTest – Basistest zur Überprüfung der Erkennung der Testumgebung.
+ *
+ * @author uhupo
+ * @version 1.0
+ */
+class SmokeTest {
+    @Test
+    void shouldRun() {
+        System.out.println("SmokeTest running...");
+        assertTrue(false);
+    }
+
+}
