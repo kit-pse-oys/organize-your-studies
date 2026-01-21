5b3c265685c22c5b8cc3006aac3a911f9a67257f
Dav <120960057+blckvsn@users.noreply.github.com>
2026-01-21 11:25:51 +0100

    Add initial AuthService class for identity and session management

---
+++ java-server/src/main/java/de/pse/oys/service/AuthService.java
@@ -0,0 +1,14 @@
+package de.pse.oys.service;
+
+/**
+ * AuthService – Der Service für die Identitäts- und zentrale Sitzungsverwaltung.
+ * Seine Kernfunktionen umfassen:
+ *  - Provider-Management: Verwaltung der lokalen und externen Authentifizierung (OAuth2).
+ *  - Validierung externer Identitäten: Sicherstellung der Authentizität externer Benutzer.
+ *  - Sitzungsverwaltung(JWT): Nach erfolgreicher Authentifizierung Erzeugung von Sitzungstokens.
+ *
+ * @author uhupo
+ * @version 1.0
+ */
+public class AuthService {
+}
