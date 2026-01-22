c0e94c2ef901140b94d73f8a6fdcf297d04cec60
Dav <120960057+blckvsn@users.noreply.github.com>
2026-01-22 21:08:59 +0100

    added getId javadoc in User.java
    added LoginDTO.java skeleton

--- java-server/src/main/java/de/pse/oys/domain/User.java
+++ java-server/src/main/java/de/pse/oys/domain/User.java
@@ -154,6 +154,10 @@ public abstract class User {
 
     // GETTER
 
+    /**
+     * Gibt die user id
+     * @return die user id als UUID
+     */
     public UUID getId() {
         return userId;
     }
