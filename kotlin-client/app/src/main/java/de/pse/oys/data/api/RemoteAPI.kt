548b15346c1ef79751d001c83b823762626fc69d
Noah Dörr <archecraft1@gmail.com>
2026-02-06 13:41:41 +0100

    Fix plan API

--- kotlin-client/app/src/main/java/de/pse/oys/data/api/RemoteAPI.kt
+++ kotlin-client/app/src/main/java/de/pse/oys/data/api/RemoteAPI.kt
@@ -298,7 +298,7 @@ internal constructor(
         withContext(Dispatchers.IO) {
             client.get(serverUrl) {
                 url {
-                    apiPath("plan")
+                    apiPath("plan/units")
                 }
             }.responseAs()
         }
