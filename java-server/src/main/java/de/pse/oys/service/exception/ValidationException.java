1de986a753fce490d60c51bbe056bcdf3d1313c0
Marcel Herm <uqvfm@kit.edu>
2026-03-09 21:54:07 +0100

    Ungenutzte Exceptions gelöscht

--- java-server/src/main/java/de/pse/oys/service/exception/ValidationException.java
+++ java-server/src/main/java/de/pse/oys/service/exception/ValidationException.java
@@ -1,16 +1,7 @@
 package de.pse.oys.service.exception;
 
 public class ValidationException extends RuntimeException {
-
-    public ValidationException() {
-        super("Validierung fehlgeschlagen.");
-    }
-
     public ValidationException(String message) {
         super(message);
-    }
-
-    public ValidationException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
