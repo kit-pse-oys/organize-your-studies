d46c374dfe7a2a4af1ba2bee264f4ec5c2a07621
urfmo <urfmo@student.kit.edu>
2026-03-22 14:17:19 +0100

    bug fix

--- kotlin-client/app/src/main/java/de/pse/oys/ui/view/MainView.kt
+++ kotlin-client/app/src/main/java/de/pse/oys/ui/view/MainView.kt
@@ -281,7 +281,7 @@ private fun UpcomingUnitsList(viewModel: IMainViewModel, modifier: Modifier = Mo
 @Composable
 private fun OnEventClick(viewModel: IMainViewModel, clickedEvent: PlannedUnit?, onDismiss: () -> Unit) {
     clickedEvent?.let { unit ->
-        Dialog({ onDismiss }) {
+        Dialog({ onDismiss() }) {
             Box(
                 modifier = Modifier
                     .background(
@@ -328,7 +328,7 @@ private fun OnEventClick(viewModel: IMainViewModel, clickedEvent: PlannedUnit?, 
                     if (now in start..end) {
                         SimpleButtonSmall(stringResource(R.string.mark_as_finished_early_button)) {
                             viewModel.marksAsFinished(unit)
-                            onDismiss
+                            onDismiss()
                         }
                     }
                 }
